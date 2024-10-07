b05a3db8bb2e05109fc4913b9adc51e7c58ea924
Abhishekbharambe <123597077+AbhishekBharambe2002@users.noreply.github.com>
2024-10-07 14:30:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,22 @@ const PORT =5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+//Deployment//
+
+const __dirname1 = path.resolve();
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
+//Deployment//

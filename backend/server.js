50b924025b9501d794c18eb840fd2551a3de9bcc
Abhishekbharambe <123597077+AbhishekBharambe2002@users.noreply.github.com>
2024-10-07 15:09:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,11 +34,11 @@ const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   // Serve static files from the React app's build folder
-  app.use(express.static(path.join(__dirname1, "frontend", "build")));
+  app.use(express.static(path.join(__dirname1, "../frontend", "build")));
 
   // Handle any requests that don't match an API route
   app.get("*", (req, res) => 
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "../frontend", "build", "index.html"))
   );
 } else {
   // Send a simple response in development mode
